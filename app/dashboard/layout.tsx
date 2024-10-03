ba432ea70467c66c0d68ef3927ddff291f68fd1d
Deni Dharmatin <deni.dharmatin@99.co>
2024-10-03 20:12:34 +0700

    initial commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,9 @@
 import SideNav from "@/app/ui/dashboard/sidenav"
 import React from "react"
 
-export default function Layout({children}: {children: React.ReactNode}) {
+export const experimental_ppr = true
+
+export default async function Layout({children}: {children: React.ReactNode}) {
     return(
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
